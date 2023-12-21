b81e373bedb24484792cf0931e9c39dcbde2d715
thilinagamage <thilinamadu658@gmail.com>
2023-12-21 17:08:32 +0530

    Google fonts added

--- functions.php
+++ functions.php
@@ -10,6 +10,8 @@
      */
 function devt_load_css(){
     wp_enqueue_style('boostrap-css',  get_template_directory_uri() . '/css/bootstrap.min.css', array(), '1.0', 'all');
+    wp_enqueue_style('style-css', get_stylesheet_uri() , array(), '1.0', 'all');
+    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap', array(), null);
 }
 
 add_action('wp_enqueue_scripts', 'devt_load_css');
