4765cda9a7fc1f9d2ad0825c66995070bafcc65c
thilinagamage <thilinamadu658@gmail.com>
2023-12-21 12:14:22 +0530

    Initial commit

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> >
+<head>
+    <meta charset="<?php bloginfo(); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WP DEV</title>
+</head>
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+    
+    <header>
+        <section class="top-bar">
+            <div class="ste-logo">
+                    <h1>LOGO</h1>
+            </div>
+            <div class="searchbar">
+                <h2>Search</h2>
+            </div>
+        </section>
+
+        <section class="menu-area0">
+            <div class="main-menu">
+                menu
+            </div>
+        </section>
+    </header> 
---
+++ index.php
@@ -0,0 +1,20 @@
+
+<?php get_header(); ?>
+
+    <div id="content" class="site-content">
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main">
+                <section class="hero">
+                    Hero
+                </section>
+                <section class="services">
+                        services
+                </section>
+                <section class="blog">
+                        blog
+                </section>
+            </main>
+        </div>
+    </div>
+
+<?php get_footer();?>
